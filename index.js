b2fcfe147c11052eae11a90b1ad963699cccd219
Rafid-Al-Ahsan <rafidahsan97@gmail.com>
2024-05-31 19:57:01 +0600

    create-payment-intent

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 5001
 const cors = require('cors')
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -29,6 +30,27 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+const verifyJWT = (req, res, next) => {
+     console.log('hitting verify jwt');
+     console.log(req.headers.authorization);
+     const authorization = req.headers.authorization;
+
+     if(!authorization){
+         return res.status(401).send({error: true, message: 'unauthorized access'})
+     }
+     const token = authorization.split(' ')[1];
+     console.log('token:', token);
+
+     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error,decoded) => {
+          if(error){
+              return res.status(403).send({error: true, message: 'unauthorized access'})
+          }
+          req.decoded = decoded;
+          next();
+     })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -40,11 +62,11 @@ async function run() {
     const usersCollection = client.db('summerCamp').collection('users');
 
     //jwt 
-    app.post('/jwt', (req,res) => {
-       const user = req.body;
-       console.log(user);
-       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
-       res.send({ token });
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
     })
 
     // classes collection
@@ -60,77 +82,86 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/classes/email/:instructoremail', async (req, res) => {
+    app.get('/classes/email/:instructoremail', verifyJWT, async (req, res) => {
+      // console.log(req.headers.authorization);
+
+      // optional: if you want to make it more secure
+      const decoded = req.decoded;
+      console.log('came back after verify', decoded);
+      if(decoded.email !== req.params.instructoremail){
+          return res.status(403).send({error: 1, message: 'forbidden access'})
+      }
+
       const instructorEmail = req.params.instructoremail;
       const user = await userClasses.find({ Email: instructorEmail }).toArray();
       res.send(user);
     });
 
-    app.get('/classes/:id', async (req, res) => {
-      const Id = req.params.id;
-      const user = await userClasses.findOne({ _id: new ObjectId(Id)});
-      res.send(user);
-    });
+    // app.get('/classes/:id', async (req, res) => {
+    //   const Id = req.params.id;
+    //   const user = await userClasses.findOne({ _id: new ObjectId(Id) });
+    //   res.send(user);
+    // });
 
-    app.get('/classes/:id/Feedback', async (req, res) => {
-      // const Id = req.params.id;
-      // const user = await userClasses.find({ _id: new ObjectId(Id)}).toArray();
-      // res.send(user);
+    // app.get('/classes/:id/Feedback', async (req, res) => {
+    //   // const Id = req.params.id;
+    //   // const user = await userClasses.find({ _id: new ObjectId(Id)}).toArray();
+    //   // res.send(user);
 
-      const Id = req.params.id;
-      const user = await userClasses.find({ _id: new ObjectId(Id) }).project({ Feedback: 1 }).toArray();
+    //   const Id = req.params.id;
+    //   const user = await userClasses.find({ _id: new ObjectId(Id) }).project({ Feedback: 1 }).toArray();
 
-      // If you want to send only the Feedback field, you can map the result
-      const feedback = user.map(u => u.Feedback);
+    //   // If you want to send only the Feedback field, you can map the result
+    //   const feedback = user.map(u => u.Feedback);
 
-      res.send(feedback);
-    });
+    //   res.send(feedback);
+    // });
 
-    app.put('/classes/:id', async(req,res) => {
-      const id = req.params.id;
-      const classes = req.body; 
-      const filter = {_id: new ObjectId(id)};
-      const options = {upsert: true};
-      const updatedUser = {
-        $set: {
-            ClassName: classes.classname,
-            Img: classes.img,     
-            Seats: classes.price,
-            Price: classes.seat,
-        }
-      }
-      const result = await userClasses.updateOne(filter, updatedUser, options);
-      res.send(result);
-   })
-
-    app.put('/classes/feedback/:id', async(req,res) => {
-      const id = req.params.id;
-      const classes = req.body; 
-      const filter = {_id: new ObjectId(id)};
-      const options = {upsert: true};
-      const updatedUser = {
-        $set: {
-            Feedback: classes.feedback,
-        }
-      }
-      const result = await userClasses.updateOne(filter, updatedUser, options);
-      res.send(result);
-   })
-
-    app.put('/classes/status/:id', async(req,res) => {
+    app.put('/classes/:id', async (req, res) => {
       const id = req.params.id;
       const classes = req.body;
-      console.log(classes); 
-      const filter = {_id: new ObjectId(id)};
-      const options = {upsert: true};
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
       const updatedUser = {
         $set: {
-            Status: classes.value,
+          ClassName: classes.classname,
+          Img: classes.img,
+          Seats: classes.price,
+          Price: classes.seat,
         }
       }
       const result = await userClasses.updateOne(filter, updatedUser, options);
       res.send(result);
-   })
+    })
+
+    app.put('/classes/feedback/:id', async (req, res) => {
+      const id = req.params.id;
+      const classes = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedUser = {
+        $set: {
+          Feedback: classes.feedback,
+        }
+      }
+      const result = await userClasses.updateOne(filter, updatedUser, options);
+      res.send(result);
+    })
+
+    app.put('/classes/status/:id', async (req, res) => {
+      const id = req.params.id;
+      const classes = req.body;
+      console.log(classes);
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedUser = {
+        $set: {
+          Status: classes.value,
+        }
+      }
+      const result = await userClasses.updateOne(filter, updatedUser, options);
+      res.send(result);
+    })
 
 
 
@@ -141,16 +172,16 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/carts', async (req, res) => {
-      const cursor = cartCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    });
+    // app.get('/carts', async (req, res) => {
+    //   const cursor = cartCollection.find();
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // });
 
-    app.get('/carts/:email', async (req,res) => {
-        const userEmail = req.params.email;
-        const user = await cartCollection.find({ UserEmail: userEmail}).toArray();
-        res.send(user);
+    app.get('/carts/:email', async (req, res) => {
+      const userEmail = req.params.email;
+      const user = await cartCollection.find({ UserEmail: userEmail }).toArray();
+      res.send(user);
     })
 
 
@@ -163,9 +194,9 @@ async function run() {
     })
 
     //users collection
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyJWT,  async (req, res) => {
       const cursor = usersCollection.find();
-      const result = await cursor.toArray();
+      const result = await cursor.toArray();  
       res.send(result);
     });
 
@@ -190,19 +221,43 @@ async function run() {
       res.send(result);
     })
 
-    app.put('/users/:id', async(req,res) => {
+    app.put('/users/:id', async (req, res) => {
       const id = req.params.id;
       const classes = req.body;
-      const filter = {_id: new ObjectId(id)}; 
-      const options = {upsert: true};
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
       const updatedUser = {
         $set: {
-            role: classes.value,
+          role: classes.value,
         }
       }
       const result = await usersCollection.updateOne(filter, updatedUser, options);
       res.send(result);
-   })
+    })
+
+    app.get('/users/:email', async (req, res) => {
+      const userEmail = req.params.email;
+      const user = await usersCollection.find({ email: userEmail }).toArray();
+      const role = user.map(u => u.role);
+
+      res.send(role);
+    })
+
+    // create payment intent
+    app.post('/create-payment-intent',  async(req, res) => {
+       const {price} = req.body;
+
+       const amount = price*100;
+       console.log(price, amount);
+       const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: 'usd',
+          payment_method_types: ['card']
+       });
+       res.send({
+          clientSecret: paymentIntent.client_secret
+       })
+    })
 
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
